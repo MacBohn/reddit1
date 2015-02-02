a5df5c840eb6a2e2a01c6d08bb200505a130b320
McCartney Bohn <mccartneybohn@yahoo.com>
2015-02-02 15:00:42 -0700

    added css and fixed nav to say the category viewing

---
+++ app/routes/posts.js
@@ -0,0 +1,14 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+
+  model: function(params) {
+    var id = params.subreddit_id;
+    return Ember.$.getJSON('http://www.reddit.com/r/' + id + '.json', function(res) {
+      return res.data.children;
+    });
+  },
+  UpperCase: function(str){
+    return str.toUpperCase()
+  }
+});
